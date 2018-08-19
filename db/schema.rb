72ed8bd2d8e0c6f70a088b5908d7b0cd16d51a3a
sun-vm <jmw686@nyu.edu>
2018-08-19 09:49:09 -0500

    add recipes resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_19_144333) do
+ActiveRecord::Schema.define(version: 2018_08_19_144750) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2018_08_19_144333) do
     t.string "units"
     t.decimal "price"
     t.string "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
