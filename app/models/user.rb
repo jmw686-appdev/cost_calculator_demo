f8351fce52b5da816e457df2ee85f0cd2adc57a6
sun-vm <jmw686@nyu.edu>
2018-08-19 10:21:22 -0500

    add model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ has_many :recipes, dependent: :destroy
+ validates :username, presence: true, uniqueness: true
 end
